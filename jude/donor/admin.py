2132c7035e3c928bda58152a57bb115adbdbeee9
parth <parthshah590@gmail.com>
2020-07-25 22:30:32 +0530

    donnor admin updated

--- jude/donor/admin.py
+++ jude/donor/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from donor.models import donor_table, patient_table 
+
+admin.site.register(donor_table)
+admin.site.register(patient_table)
