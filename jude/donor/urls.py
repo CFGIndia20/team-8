3702cc7c19205a1fd2c9ccac90289e15999de5a4
priaga20 <priyanshi.agarwal3405@gmail.com>
2020-07-25 23:15:20 +0530

    Bugs fixed

--- jude/donor/urls.py
+++ jude/donor/urls.py
@@ -1,4 +1,6 @@
 #hello
+from django.urls import path
+from donor import views
 app_name='donor'
 urlpatterns = [
 	path('sms_reply', views.sms_reply, name='sms_reply'),
--- jude/donor/views.py
+++ jude/donor/views.py
@@ -1,4 +1,4 @@
-
+from django.http import HttpResponse
 # Create your views here.
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
