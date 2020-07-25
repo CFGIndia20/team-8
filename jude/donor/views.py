cd15f86c062559be3e90e6fc9d7780695205a510
parth <parthshah590@gmail.com>
2020-07-25 22:33:44 +0530

    donnor views sms_reply

--- jude/donor/views.py
+++ jude/donor/views.py
@@ -1,2 +1,12 @@
 
 # Create your views here.
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    msg = request.form.get('Body')
+    
+    # Create reply
+    resp = MessagingResponse()
+    resp.message("You said: {}".format(msg))
+
+    return str(resp)
