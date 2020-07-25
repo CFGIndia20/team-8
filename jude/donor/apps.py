84d1048d37413b301a1f55d7b38cde13af2c4d7b
Smruti <smruti22oct@gmail.com>
2020-07-25 21:37:24 +0530

    deleting

--- jude/donor/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DonorConfig(AppConfig):
-    name = 'donor'
--- jude/feedback/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, re_path
-from feedback import views
-
-app_name='feedback'
-urlpatterns = [
-	path('feedback_processing', views.feedback_processing, name='feedback_processing'),
-]
